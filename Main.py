3026746b4b08a64976ba9869407328d377fe221c
Adamsen5482 <tadams17@student.aau.dk>
2019-03-19 10:16:39 +0100

    Warning is no longer showing up

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import scipy.io.wavfile as sp
 import numpy as np
 import matplotlib.pyplot as plt
+import warnings
 
 
 # Takes a numpy array as an input
@@ -36,11 +37,14 @@ def plot(array, time):
     plt.show()
 
 
-# Write Audio Files/ and then the name of the intended audio file
-sampleRate, samples = sp.read("Audio Files/Tone_2.wav")
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")  # ignore annoying WavFileWarning
+    sampleRate, samples = sp.read("Audio Files/Tone_2.wav")  # Write Audio Files and the name of the intended audio file
+
+
 #  SampleRate = number of samples per second and samples = 2D Array
 print("SampleRate: ", sampleRate, " Number of samples: ", len(samples))
-data = np.fft.fft(samples) # Returns 2D array with complex numbers
+data = np.fft.fft(samples)  # Returns 2D array with complex numbers
 time = data.shape[0]/sampleRate
 print("Time: ", time)
 freq = convert(data, time)
