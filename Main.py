79a5035798798e136e884a74973be3b8b7989955
Alexcool3 <Alex33@live.dk>
2019-03-19 13:46:50 +0100

    Pitch estimation and comparesion
    Better pitch estimation and the start of pitch comparesion, but still needs more work

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ import scipy.io.wavfile as sp
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")  # Ignore annoying WavFileWarning
-    sampleRate, samples = sp.read("Audio Files/Tone_2.wav")  # Read Audio Files and the name of the intended audio file
+    sampleRate, samples = sp.read("Audio Files/3_tones.wav")  # Read Audio Files and the name of the intended audio file
 
 #  SampleRate = number of samples per second and samples = 2D Array
 print("SampleRate: ", sampleRate, " Number of samples: ", len(samples))
@@ -17,8 +17,17 @@ data = np.fft.fft(samples)  # Returns 2D array with complex numbers
 time = data.shape[0]/sampleRate
 print("Time: ", time)
 freq = pe.convert(data, time)
+print("Data: ", data[1])
+print("Data: ", data[2])
+print("Data: ", data[3])
+print("freq: ", freq[1])
 print("freq: ", freq[2])
+print("freq: ", freq[3])
+print("freq: ", freq[4])
 
+
+
+pe.pitchCompare(freq)
 
 
 
--- Pitch_Estimation.py
+++ Pitch_Estimation.py
@@ -1,12 +1,15 @@
 import scipy.io.wavfile as sp
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+import random
 import warnings
 
 # Takes a numpy array as an input
 def convert(array, duration):
     i = 0
-    k = 0.64  # The interval between  the base frequency
+    k = 0.6264  # The interval between  the base frequency
+    complexNum = complex(0+0j)
     # Assigns frequency to empty array with the data type complex numbers
     frequency = np.empty(shape=(len(array), 1), dtype=np.complex)
     while i < 1:  # x dimension in input array
@@ -18,12 +21,59 @@ def convert(array, duration):
                 #  Base frequency equals element in fft array divide by the scalar which is time duration times interval between frequencies
                 frequency[j, i] = array[j, i]/scalar  # assigns an element in the frequency array to be equal to the base frequency.
                 #print("Array: ", array[j])
+            k += 0.645
+            if k >= 7.076399999999998:
+              k = 0.6264
             #print("i: ", i, "j: ", j)
-            k += 0.64
             j += 1
+
+
+
         i += 1
 
     return frequency
+
+
+def pitchCompare(array):
+
+    i = 0
+    tmp = np.empty(shape=(len(array), 1), dtype=np.complex)
+    min = complex(80+0j)
+    max = complex(82+0j)
+
+    min1 = complex(260 + 0j)
+    max1 = complex(263 + 0j)
+
+    min2 = complex(1900 + 0j)
+    max2 = complex(2050 + 0j)
+
+
+    max3 = complex(0 + 0j)
+
+    while i < 1:
+        j = 0
+        while j < len(array):
+            if min < array[j, i] < max:
+                print('Freq: 80-82', j)
+                tmp = array[j,i]
+                print("Temp is: ", tmp)
+            elif min1 < array[j, i] < max1:
+                print('Freq: 260-263', j)
+                tmp = array[j,i]
+                print("Temp is: ", tmp)
+            elif min2 < array[j, i] < max2:
+                print('Freq: 1900-2050',j)
+                tmp = array[j,i]
+                print("Temp is: ", tmp)
+            #elif  array[j, i] == max3:
+             #   tmp = array[j,i]
+                #   if random.randint(1,2) == 2:
+                 #   print("No audio")
+                  #  print(tmp)
+            j += 1
+        i += 1
+    return tmp
+
 
 
 def interval(freq, time):
@@ -37,5 +87,7 @@ def plot(array, time):
     plt.ylabel('Frequency')
     plt.xlabel('Interval')
     plt.show()
+
+
 
 
